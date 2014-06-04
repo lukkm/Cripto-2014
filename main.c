11c963a1c32c30b05dcc2ccf9410ff7cf401754c
Lucas Moscovicz <lmoscovicz@Lucass-MacBook-Pro.local>
2014-06-04 18:54:50 -0300

    Finished image parser

--- main.c
+++ main.c
@@ -1,23 +1,49 @@
 #include <stdio.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
+#include <stdlib.h>
+#include "main.h"
 
 int
 main(int argc, char **argv)
 {
     int i, j;
 
-    if (argc < 2) {
-        printf("Usage: %s filename.\n", argv[0]);
+    if (argc < 4) {
+        printf("Usage: %s filename width height.\n", argv[0]);
         return 1;
     }
 
-    FILE * f = fopen(argv[1], 'r');
+    FILE * f = fopen(argv[1], "rw");
+    image_t * bmpimage = malloc(sizeof(image_t));
+    bmpimage->width = atoi(argv[2]); 
+    bmpimage->height = atoi(argv[3]);
+    bmpimage->data = malloc(bmpimage->width * sizeof(pixel_t *));
 
-    struct stat buf;
-    fstat(f, &buf);
-    printf("Size: %d\n", buf.st_size);
+    // Get the total size in bytes of the image (including header)
+    fseek(f, 0L, SEEK_END);
+    int total_size = ftell(f);
+
+    printf("%d\n", bmpimage->width);
+    for (i = 0; i < bmpimage->width; i++) {
+        bmpimage->data[i] = malloc(bmpimage->height);
+    }
+
+    // Seek to the beggining of the bitmap matrix
+    long seek_to = total_size - (bmpimage->width * bmpimage->height);
+    fseek(f, seek_to, SEEK_SET);
+
+
+    for (i = 0; i < bmpimage->width; i++) {
+        for (j = 0; j < bmpimage->height; j++) {
+            size_t read = fread(&bmpimage->data[i][j], sizeof(pixel_t), 1, f);
+        }
+    }
+    
+    for (i = 0; i < bmpimage->width; i++) {
+        for (j = 0; j < bmpimage->height; j++) {
+            printf("%d ", bmpimage->data[i][j]);
+        }
+        printf("\n");
+    }
 
     return 0;
 }
--- main.h
+++ main.h
@@ -1,2 +1,11 @@
 typedef unsigned char pixel_t;
-typedef pixel_t ** image_t; 
+
+#ifndef uint32_t
+typedef unsigned int uint32_t;
+#endif
+
+typedef struct {
+    uint32_t width;
+    uint32_t height;
+    pixel_t ** data;
+} image_t; 
