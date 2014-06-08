2efa09bc596bc3a4e37c241c93dbc7331483cb80
Lucas Moscovicz <lmoscovicz@Lucass-MacBook-Pro.local>
2014-06-08 18:40:43 -0300

    modifications

--- main.h
+++ main.h
@@ -7,5 +7,11 @@ typedef unsigned int uint32_t;
 typedef struct {
     uint32_t width;
     uint32_t height;
+    uint32_t header_size;
+    char * header;
     pixel_t ** data;
-} image_t; 
+} image_t;
+
+void BMP_write(image_t * image);
+image_t * BMP_read(char * filename, uint32_t width, uint32_t height);
+void BMP_print_matrix(image_t * bmpimage);
