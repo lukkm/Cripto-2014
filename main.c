90aa5b3251152c8a3c0487dc10080c42094b6224
Agustin Pagnoni <aguspagnoni@hotmail.com>
2014-06-08 20:34:56 -0300

    bmp correctly read

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <argtable2.h>
+// #include <argtable2.h>
+#include "bmp.h"
 #include "main.h"
 
 int
@@ -13,13 +14,18 @@ main(int argc, char **argv)
         return 1;
     }
 
-    image_t * bmpimage = BMP_read(argv[1], atoi(argv[2]), atoi(argv[3]));
-    BMP_write(bmpimage);
+    //image_t * bmpimage = BMP_read(argv[1], atoi(argv[2]), atoi(argv[3]));
+
+    BITMAPINFOHEADER bitmapInfoHeader;
+    unsigned char *bitmapData;
+    bitmapData = LoadBitmapFile(argv[1],&bitmapInfoHeader);
+
+    // BMP_write(bmpimage);
 
     return 0;
 }
 
-image_t * 
+image_t *
 BMP_read(char * filename, uint32_t width, uint32_t height) {
     int i, j;
 
@@ -28,11 +34,11 @@ BMP_read(char * filename, uint32_t width, uint32_t height) {
     bmpimage->width = width;
     bmpimage->height = height;
     bmpimage->data = malloc(bmpimage->width * sizeof(pixel_t *));
-    
+
     // Get the total size in bytes of the image (including header)
     fseek(f, 0L, SEEK_END);
     int total_size = ftell(f);
-    
+
     printf("%d\n", total_size);
     printf("%d\n", bmpimage->width * bmpimage->height);
     bmpimage->header_size = total_size - (bmpimage->width * bmpimage->height);
@@ -58,13 +64,13 @@ BMP_read(char * filename, uint32_t width, uint32_t height) {
     return bmpimage;
 }
 
-void 
+void
 BMP_write(image_t * image) {
     int i, j;
 
     FILE *file;
     file = fopen("generated_bitmap.bmp", "w+");
-  
+
 
     fwrite(image->header, image->header_size, 1, file);
 
